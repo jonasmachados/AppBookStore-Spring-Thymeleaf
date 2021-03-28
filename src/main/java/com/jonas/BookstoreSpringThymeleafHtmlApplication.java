2d3b126aa844c797db91d2da23925c9d1d0731eb
jonasmachados <jonasmachado.ti@gmail.com>
2021-03-28 20:14:08 -0300

    Project created

---
+++ src/main/java/com/jonas/BookstoreSpringThymeleafHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.jonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringThymeleafHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSpringThymeleafHtmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/BookstoreSpringThymeleafHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSpringThymeleafHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
