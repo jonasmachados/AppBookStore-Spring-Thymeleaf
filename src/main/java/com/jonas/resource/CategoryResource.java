109f23939eca7b2f3025ca35b73c266d0d5dcd10
jonasmachados <jonasmachado.ti@gmail.com>
2021-03-31 22:31:20 -0300

    BACKEND - Method to created and update 'category'

--- src/main/java/com/jonas/resource/CategoryResource.java
+++ src/main/java/com/jonas/resource/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.jonas.resource;
 
 import com.jonas.domain.Category;
+import com.jonas.exception.RecordNotFoundException;
 import com.jonas.service.CategoryService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -25,5 +29,23 @@ public class CategoryResource {
         List<Category> list = service.findAllCategory();
         model.addAttribute("categories", list);
         return "category/list-category";
+    }
+
+    @RequestMapping(path = {"/edit", "/edit/{id}"})
+    public String editCategoryById(Model model, @PathVariable("id") Optional<Integer> id)
+            throws RecordNotFoundException {
+        if (id.isPresent()) {
+            Category entity = service.getCategoryById(id.get());
+            model.addAttribute("category", entity);
+        } else {
+            model.addAttribute("category", new Category());
+        }
+        return "category/add-edit-category";
+    }
+
+    @RequestMapping(path = "/createCategory", method = RequestMethod.POST)
+    public String createOrUpdateCategory(Category category) {
+        service.createOrUpdateCategory(category);
+        return "redirect:/categories"; //REDIRECT: back to previous HTML.
     }
 }
--- src/main/java/com/jonas/service/CategoryService.java
+++ src/main/java/com/jonas/service/CategoryService.java
@@ -1,9 +1,11 @@
 package com.jonas.service;
 
 import com.jonas.domain.Category;
+import com.jonas.exception.RecordNotFoundException;
 import com.jonas.repositories.CategoryRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,40 @@ public class CategoryService {
             return result;
         } else {
             return new ArrayList<Category>();
+        }
+    }
+    
+     public Category getCategoryById(Integer id) throws RecordNotFoundException {
+        Optional<Category> category = repository.findById(id);
+
+        if (category.isPresent()) {
+            return category.get();
+        } else {
+            throw new RecordNotFoundException("No category record exist for given id");
+        }
+    }
+
+    public Category createOrUpdateCategory(Category entity) {
+        if (entity.getId() == null) {
+            entity = repository.save(entity);
+
+            return entity;
+        } else {
+            Optional<Category> category = repository.findById(entity.getId());
+
+            if (category.isPresent()) {
+                Category newCategory = category.get();
+                newCategory.setName(entity.getName());
+                newCategory.setDescription(entity.getDescription()); 
+
+                newCategory = repository.save(newCategory);
+
+                return newCategory;
+            } else {
+                entity = repository.save(entity);
+
+                return entity;
+            }
         }
     }
 
