436496a0b9827bf7fa889c89e888de903c2e2bfd
jonasmachados <jonasmachado.ti@gmail.com>
2021-04-13 21:58:54 -0300

    Configure Logout

--- src/main/java/com/jonas/config/SecurityConfiguration.java
+++ src/main/java/com/jonas/config/SecurityConfiguration.java
@@ -11,9 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
-
-
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
